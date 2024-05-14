1b9421176c97587063299bad47d3eb76ebee9507
vicky <prajwalkawale01@hotmail.com>
2024-05-14 16:37:46 +0530

    Day_01

---
+++ COLLEGE.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE collegeDatabase;
+USE collegeDatabase;
+CREATE TABLE student (
+id INT PRIMARY KEY,
+name VARCHAR(50),
+age INT NOT NULL
+);
+
+INSERT INTO student VALUES(1,"Shubham",26);
+INSERT INTO student VALUES(2,"clay",24);
+
+
+SELECT * FROM student;
+
+
+
